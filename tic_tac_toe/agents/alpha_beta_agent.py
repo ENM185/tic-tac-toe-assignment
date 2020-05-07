d325f7055dcce1579c81a6b7e90b294483b0c619
ENM185 <ericnathanmiller@icloud.com>
2020-05-06 20:14:52 -0400

    Split minimax into two methods (move and state)

--- tic_tac_toe/agents/alpha_beta_agent.py
+++ tic_tac_toe/agents/alpha_beta_agent.py
@@ -1,9 +1,11 @@
+from copy import deepcopy
+
 from .base_agent import valid_moves
-from .minimax_agent import MinimaxAgent
+from .minimax_agent import Agent, valid_moves
 from ..player import other_player
 
 
-class AlphaBetaAgent(MinimaxAgent):
+class AlphaBetaAgent(Agent):
     def __init__(self, player):
         super().__init__(player)
         self._states_visited_last_turn = 0
@@ -11,7 +13,7 @@ class AlphaBetaAgent(MinimaxAgent):
     def next_move(self, board):
         self._states_visited_last_turn = 1 # this counts as a state
         return max(
-            [(move,self._minimax(move, board, other_player(self._player), -2, 2)) for move in valid_moves(board, self._player)], 
+            [(move,self._minimax_move(move, board, other_player(self._player))) for move in valid_moves(board, self._player)], 
             key=lambda t: t[1] # select min by score in (move, score)
             )[0] # select move in (move, score)
 
@@ -19,13 +21,35 @@ class AlphaBetaAgent(MinimaxAgent):
     def states_visited_last_turn(self):
         return self._states_visited_last_turn
 
-    def _better_case(self, is_max_player, score, alphabeta):
-        if is_max_player:
-            if score >= alphabeta[1]:
-                return True
-            alphabeta[0] = max(alphabeta[0], score)
-        else:
-            if score <= alphabeta[0]:
-                return True
-            alphabeta[1] = min(alphabeta[1], score)
-        return False
+    def _minimax_move(self, move, board, player, alpha=-2, beta=2):
+        # update board for new "state"
+        board_copy = deepcopy(board).set_cell(move.player, move.row, move.col)
+        self._states_visited_last_turn += 1
+        return self._minimax_state(board_copy, player, alpha, beta)
+
+    def _minimax_state(self, board, player, alpha, beta):
+        # terminal cases
+        winner = board.winner
+        if winner == self._player:
+            return 1
+        if winner == other_player(self._player):
+            return -1
+        if not valid_moves(board, player):
+            return 0
+
+        # recursive case
+        multiplier = 1 if self._player == player else -1 #decide whether to use max or min
+        score = multiplier * -2
+        for next_move in valid_moves(board, player):
+            score = multiplier * max(multiplier * score, multiplier * self._minimax_move(next_move, board, other_player(player), alpha,beta))
+
+            if multiplier == 1:
+                if score >= beta:
+                    return score
+                alpha = max(alpha, score)
+            else:
+                if score <= alpha:
+                    return score
+                beta = min(beta, score)
+
+        return score
--- tic_tac_toe/agents/minimax_agent.py
+++ tic_tac_toe/agents/minimax_agent.py
@@ -1,5 +1,4 @@
 from copy import deepcopy
-from pprint import pprint
 
 from .base_agent import Agent, valid_moves
 from ..player import other_player
@@ -13,38 +12,34 @@ class MinimaxAgent(Agent):
     def next_move(self, board):
         self._states_visited_last_turn = 1 # this counts as a state
         return max(
-            [(move,self._minimax(move, board, other_player(self._player))) for move in valid_moves(board, self._player)], 
+            [(move,self._minimax_move(move, board, other_player(self._player))) for move in valid_moves(board, self._player)], 
             key=lambda t: t[1] # select min by score in (move, score)
             )[0] # select move in (move, score)
 
     @property
     def states_visited_last_turn(self):
-        return self._states_visited_last_turn      
+        return self._states_visited_last_turn
 
-    def _minimax(self, move, board, player, alpha=None, beta=None):
+    def _minimax_move(self, move, board, player):
         # update board for new "state"
         board_copy = deepcopy(board).set_cell(move.player, move.row, move.col)
         self._states_visited_last_turn += 1
+        return self._minimax_state(board_copy, player)
 
+    def _minimax_state(self, board, player):
         # terminal cases
-        if not valid_moves(board_copy, player):
-            return 0
-        winner = board_copy.winner
+        winner = board.winner
         if winner == self._player:
             return 1
         if winner == other_player(self._player):
             return -1
+        if not valid_moves(board, player):
+            return 0
 
         # recursive case
         multiplier = 1 if self._player == player else -1 #decide whether to use max or min
         score = multiplier * -2
-        for next_move in valid_moves(board_copy, player):
-            score = multiplier * max(multiplier * score, multiplier * self._minimax(next_move, board_copy, other_player(player), alpha, beta))
-            alphabeta = [alpha, beta]
-            if self._better_case(multiplier == 1, score, alphabeta):
-                return score
-            alpha, beta = alphabeta
+        for next_move in valid_moves(board, player):
+            score = multiplier * max(multiplier * score, multiplier * self._minimax_move(next_move, board, other_player(player)))
         return score
-
-    def _better_case(self, *args):
-        return False
+        
