2bcc6f530113d07611a721613481a4c91fafb711
ENM185 <ericnathanmiller@icloud.com>
2020-05-09 16:26:29 -0400

    Prioritized early wins

--- tic_tac_toe/agents/minimax_agent.py
+++ tic_tac_toe/agents/minimax_agent.py
@@ -22,24 +22,24 @@ class MinimaxAgent(Agent):
     def states_visited_last_turn(self):
         return self._states_visited_last_turn
 
-    def _minimax_move(self, move, board, player):
+    def _minimax_move(self, move, board, player, depth=0):
         # update board for new "state"
         board.set_cell(move.player, move.row, move.col)
         self._states_visited_last_turn += 1
-        score = self._minimax_state(board, player)
+        score = self._minimax_state(board, player, depth+1)
         board.set_cell(CellState.EMPTY, move.row, move.col)
         return score
 
-    def _minimax_state(self, board, player):
+    def _minimax_state(self, board, player, depth):
         if self._hash(board) in self._cached_states:
             return self._cached_states[self._hash(board)]
 
         # terminal cases
         winner = board.winner
         if winner == self._player:
-            return 1
+            return 1/depth
         if winner == other_player(self._player):
-            return -1
+            return -1/depth
         if not valid_moves(board, player):
             return 0
 
@@ -47,7 +47,7 @@ class MinimaxAgent(Agent):
         multiplier = 1 if self._player == player else -1 #decide whether to use max or min
         score = multiplier * -2
         for next_move in valid_moves(board, player):
-            score = multiplier * max(multiplier * score, multiplier * self._minimax_move(next_move, board, other_player(player)))
+            score = multiplier * max(multiplier * score, multiplier * self._minimax_move(next_move, board, other_player(player),depth))
         self._cached_states[self._hash(board)] = score
         return score
         
