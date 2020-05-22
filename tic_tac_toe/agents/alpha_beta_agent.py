c20eaa655420adf056b7b2f3cdec2a37cb1ed2b1
ENM185 <ericnathanmiller@icloud.com>
2020-05-22 00:51:08 -0400

    Fixed alpha/beta and heuristics

--- tic_tac_toe/agents/alpha_beta_agent.py
+++ tic_tac_toe/agents/alpha_beta_agent.py
@@ -15,14 +15,20 @@ class AlphaBetaAgent(Agent):
 
     def next_move(self, board):
         self._states_visited_last_turn = 1 # this counts as a state
-        return max(
-            [(move,self._minimax_move(move, board, other_player(self._player))) for move in valid_moves(board, self._player)], 
-            key=lambda t: t[1] # select min by score in (move, score)
-            )[0] # select move in (move, score)
+        return self._minimax(board)
 
     @property
     def states_visited_last_turn(self):
         return self._states_visited_last_turn
+
+    def _minimax(self, board):
+        moves = valid_moves(board, self._player)
+        moves.sort(key=lambda move: -self._evaluate_move(board, self._player, move))
+
+        return max(
+            [(move,self._apply_multiplier(-1,self._minimax_move(move, board, other_player(self._player)))) for move in moves], 
+            key=lambda t: t[1] # select min by score in (move, score)
+            )[0] # select move in (move, score)
 
     def _minimax_move(self, move, board, player, depth=0, alpha=-2, beta=2):
         # update board for new "state"
@@ -32,38 +38,32 @@ class AlphaBetaAgent(Agent):
         board.set_cell(CellState.EMPTY, move.row, move.col)
         return score
 
-    def _minimax_state(self, board, player, depth, alpha, beta):
+    def _minimax_state(self, board, player, depth, alpha, beta, pruned=False):
         if hash(board) in self._cached_leaf_nodes:
             return self._cached_leaf_nodes[hash(board)]
 
         # check terminal cases
         winner = board.winner
-        if winner == self._player:
-            return (1, depth)
-        if winner == other_player(self._player):
-            return (-1, depth)
-        if not valid_moves(board, self._player):
-            return (0, depth)
+        if winner is not None:
+            return (-1, -depth) #player before this just won
+        if not valid_moves(board, player):
+            return (0, -depth)
 
         # recursive case
-        multiplier = 1 if self._player == player else -1 #decide whether to use max or min
-        score = (multiplier * -2, 0)
-
+        score = (-2, 0)
         moves = valid_moves(board, player)
         moves.sort(key=lambda move: -self._evaluate_move(board, player, move))
 
         for next_move in moves:
+            
+            score = max(score,
+                    self._apply_multiplier(-1,self._minimax_move(next_move, board, other_player(player),depth,-beta,-alpha)))
 
-            score = self._apply_multiplier(multiplier, max(
-                self._apply_multiplier(multiplier, score), 
-                self._apply_multiplier(multiplier, 
-                    self._minimax_move(next_move, board, other_player(player),depth,-beta,-alpha))))
-
-            if multiplier * score[0] >= beta:
+            if score[0] >= beta:
                 return score
-            alpha = max(alpha, multiplier * score[0])
+            alpha = max(alpha, score[0])
 
-        self._cached_leaf_nodes[hash(board)] = score
+        #self._cached_leaf_nodes[hash(board)] = score
         return score
 
     def _apply_multiplier(self, multiplier, score):
@@ -93,7 +93,7 @@ class AlphaBetaAgent(Agent):
         
         #not terminal case
         score = 0
-        twos = [0,0] # forks: amount of lines for (other, current) that are one move away from winning
+        forks = [0,0] # amount of lines for (other, current) that are one move away from winning (2+ is a fork)
         for line in board.all_lines:
             count = [0,0] # (other, current)
             for cell in line:
@@ -102,13 +102,14 @@ class AlphaBetaAgent(Agent):
                 elif cell == other_player(player):
                     count[0] += 1
             if count[0] == 0:
-                score += 1
+                score += count[1]
             elif count[1] == 0:
-                score -= 1
-        if twos[0] > 0:
+                score -= count[0]
+            for i in range(2):
+                if count[i] == board.size-1:
+                    forks[i] += 1
+        if forks[0] > 0:
             return -10000
-        if twos[1] == 1:
-            score += 1
-        elif twos[1] == -1:
+        elif forks[1] > 1:
             return 10000
         return score
